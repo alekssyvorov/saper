72f3ae403f972d3b1db85e7cab046dd584ecc566
alekssyvorov <123986551+alekssyvorov@users.noreply.github.com>
2023-02-21 16:16:28 +0200

    Add files via upload

---
+++ Saper/Saper/Form1.cs
@@ -0,0 +1,28 @@
+﻿using Saper.Game;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Saper
+{
+    public partial class Form1 : Form
+    {
+        public const int mapSize = 8;
+        public const int cellSize = 50;
+        public static int[,] map = new int[mapSize, mapSize];
+
+        public static Button[,] buttons = new Button[mapSize, mapSize];
+        public Form1()
+        {
+            InitializeComponent();
+            MapController.Init(this);
+        }
+    }
+}
